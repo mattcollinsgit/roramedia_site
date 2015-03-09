b16305b75d77e84b4be182c04496f9d4b10783ae
damy89 <nanalartey@HL.home>
2015-03-09 00:13:31 +0000

    token

--- api/config.php
+++ api/config.php
@@ -8,5 +8,5 @@
     define('CONSUMER_SECRET', 'nuM6iS3SHHzhGdO6O1uwq8Cgbq0G2ZBePmI5NLupUZx8pE4gk9');
 
     // User Access Token
-    define('ACCESS_TOKEN', '62739365-vRagIYPlSlmdZYAgRdn3ifEuBK482KYO0tRB8qEXF');
-    define('ACCESS_SECRET', 'zkxfxAQBG2L7R05CZ0M32oUyUzTekcwR0anhlaqsB3fsH');
+    define('ACCESS_TOKEN', '2735035292-oYp8rlSp42wsmQF7BMQKTllIQ4k7O5nfw7DWBPl');
+    define('ACCESS_SECRET', 'MnmZWxrgfsGlolhkD1nm38Cpyo7LggdpuMXFrqb2jEbvn');
